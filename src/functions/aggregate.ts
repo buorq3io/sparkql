ccf122986f31496ab46fbbd8ad50070a6a2dd515
Burak Mehmet <burakmehmetekmekci@gmail.com>
2025-06-19 16:52:42 +0300

    Add all aggregation functions

--- src/functions/aggregate.ts
+++ src/functions/aggregate.ts
@@ -1,9 +1,36 @@
 import { agg } from './utils';
-import { Wildcard, ExpressionOrPrimitive } from '../struct';
+import { Wildcard } from 'sparqljs';
+import { AggregateExpression, ExpressionOrPrimitive } from '../struct';
+
+export function count(): AggregateExpression;
+export function count(expression: ExpressionOrPrimitive): AggregateExpression;
+export function count(expression?: ExpressionOrPrimitive) {
+  return agg(expression ?? new Wildcard(), 'count');
+}
+
+export function sum(expression: ExpressionOrPrimitive) {
+  return agg(expression, 'sum');
+}
+
+export function avg(expression: ExpressionOrPrimitive) {
+  return agg(expression, 'avg');
+}
+
+export function min(expression: ExpressionOrPrimitive) {
+  return agg(expression, 'min');
+}
+
+export function max(expression: ExpressionOrPrimitive) {
+  return agg(expression, 'max');
+}
+
+export function sample(expression: ExpressionOrPrimitive) {
+  return agg(expression, 'sample');
+}
 
 export function groupConcat(
-  expression: ExpressionOrPrimitive | Wildcard,
-  separator: string
+  expression: ExpressionOrPrimitive,
+  separator: string = '\u001f'
 ) {
   return agg(expression, 'group_concat', separator);
 }
